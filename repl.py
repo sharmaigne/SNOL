05dba50806d65bd233f4a66c7daff17f9f5ef71c
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-05-23 11:32:59 +0800

    feat: basic main repl loop

---
+++ repl.py
@@ -0,0 +1,29 @@
+from lexer.lexer import Lexer
+from parser.parser import Parser
+from interpreter.interpreter import Interpreter
+
+
+def main():
+    lexer = Lexer()
+    parser = Parser()
+    interpreter = Interpreter()
+
+    print("The SNOL environment is now active, you may proceed with giving your commands.")
+    while True:
+        try:
+            line = input("SNOL> ")
+            # if we have time, should add "did you mean" feature
+            if line.strip() == "EXIT!":
+                break
+            
+            tokens = lexer.tokenize(line)
+            ast = parser.parse(tokens)
+            result = interpreter.interpret(ast)
+
+            print(result)
+        except Exception as e:
+            print(e)
+    print("Interpreter is now terminated...")
+
+if __name__ == '__main__':
+    main()
