070ee2e234bc49644ea53ba1b7848a5c900c70c1
centuriee <117663072+centuriee@users.noreply.github.com>
2024-05-24 22:05:43 +0800

    fix: token splitter issues

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -69,11 +69,12 @@ class Lexer:
         # The token values will be the actual values of the tokens
 
         # split the line into tokens by spaces eg "SUM=1.45+4" -> ["SUM", "=", "1.45", "+", "4"]
-        # \d+\.[\d+]? matches floats
+        # \d+\.\d+? matches floats
+        # \d+\. matches floats with no decimal number
         # \d+ matches integers
         # [a-zA-Z0-9]+ matches words, cannot detect words starting with numbers
         # [+\-*/%()=] matches operators
-        self.tokens = re.findall(r"\d+\.[\d+]?|\d+|[a-zA-Z0-9]+|[+\-*/%()=]", line)
+        self.tokens = re.findall(r"\d+\.\d+?|\d+\.|\d+|[a-zA-Z0-9]+|[+\-*/%()=]", line)
         
         # iterate through the tokens and determine their type
         self.tokens = [self.__make_token(value) for value in self.tokens]
